60cef365d4282380f11b9786b1796c2fd7bbcf2a
VadimKt <33955117+VadimKt@users.noreply.github.com>
2018-12-21 00:17:36 +0300

    2

--- src/sample/App.java
+++ src/sample/App.java
@@ -1,16 +1,19 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.fxml.FXMLLoader;
+
 import java.io.IOException;
 
 public class App {
 
+
+    int value;
     @FXML
     private Button StopButton;
 
@@ -182,33 +185,13 @@ public class App {
     @FXML
     private Button ButtonB19;
 
+    private int[][] arrayA;
+
+    private int[][] arrayB;
+
     @FXML
-    void initialize(){
+    void initialize() {
         StopButton.setOnAction(event -> {
-          StopButton.getScene().getWindow().hide();
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("/sample/sample.fxml"));
-
-
-            try {
-                loader.load();
-            } catch (IOException e){
-                e.printStackTrace();
-            }
-            Parent root = loader.getRoot();
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root));
-            stage.showAndWait();
-
-
-        });
-
-        GameButton.setOnAction(event -> {
-            Card card = new Card();
-            int [][] game1 = card.getArray();
-            int [][] game2 = card.getArray();
-
-
             ButtonA1.setText("");
             ButtonA2.setText("");
             ButtonA3.setText("");
@@ -236,13 +219,211 @@ public class App {
             ButtonA25.setText("");
             ButtonA26.setText("");
             ButtonA27.setText("");
-
-
-
+            ButtonB1.setText("");
+            ButtonB2.setText("");
+            ButtonB3.setText("");
+            ButtonB4.setText("");
+            ButtonB5.setText("");
+            ButtonB6.setText("");
+            ButtonB7.setText("");
+            ButtonB8.setText("");
+            ButtonB9.setText("");
+            ButtonB10.setText("");
+            ButtonB11.setText("");
+            ButtonB12.setText("");
+            ButtonB13.setText("");
+            ButtonB14.setText("");
+            ButtonB15.setText("");
+            ButtonB16.setText("");
+            ButtonB17.setText("");
+            ButtonB18.setText("");
+            ButtonB19.setText("");
+            ButtonB20.setText("");
+            ButtonB21.setText("");
+            ButtonB22.setText("");
+            ButtonB23.setText("");
+            ButtonB24.setText("");
+            ButtonB25.setText("");
+            ButtonB26.setText("");
+            ButtonB27.setText("");
 
 
 
         });
+
+        GameButton.setOnAction(event -> {
+
+
+            Card cardA = new Card();
+            arrayA = cardA.getArray();
+            Card cardB = new Card();
+            arrayB = cardB.getArray();
+            if (arrayA[0][0] != 0) {
+                ButtonA1.setText(arrayA[0][0] + "");
+            }
+            if (arrayA[0][1] != 0) {
+                ButtonA2.setText(arrayA[0][1] + "");
+            }
+            if (arrayA[0][2] != 0) {
+                ButtonA3.setText(arrayA[0][2] + "");
+            }
+            if (arrayA[0][3] != 0) {
+                ButtonA4.setText(arrayA[0][3] + "");
+            }
+            if (arrayA[0][4] != 0) {
+                ButtonA5.setText(arrayA[0][4] + "");
+            }
+            if (arrayA[0][5] != 0) {
+                ButtonA6.setText(arrayA[0][5] + "");
+            }
+            if (arrayA[0][6] != 0) {
+                ButtonA7.setText(arrayA[0][6] + "");
+            }
+            if (arrayA[0][7] != 0) {
+                ButtonA8.setText(arrayA[0][7] + "");
+            }
+            if (arrayA[0][8] != 0) {
+                ButtonA9.setText(arrayA[0][8] + "");
+            }
+            if (arrayA[1][0] != 0) {
+                ButtonA10.setText(arrayA[1][0] + "");
+            }
+            if (arrayA[1][1] != 0) {
+                ButtonA11.setText(arrayA[1][1] + "");
+            }
+            if (arrayA[1][2] != 0) {
+                ButtonA12.setText(arrayA[1][2] + "");
+            }
+            if (arrayA[1][3] != 0) {
+                ButtonA13.setText(arrayA[1][3] + "");
+            }
+            if (arrayA[1][4] != 0) {
+                ButtonA14.setText(arrayA[1][4] + "");
+            }
+            if (arrayA[1][5] != 0) {
+                ButtonA15.setText(arrayA[1][5] + "");
+            }
+            if (arrayA[1][6] != 0) {
+                ButtonA16.setText(arrayA[1][6] + "");
+            }
+            if (arrayA[1][7] != 0) {
+                ButtonA17.setText(arrayA[1][7] + "");
+            }
+            if (arrayA[1][8] != 0) {
+                ButtonA18.setText(arrayA[1][8] + "");
+            }
+            if (arrayA[2][0] != 0) {
+                ButtonA19.setText(arrayA[2][0] + "");
+            }
+            if (arrayA[2][1] != 0) {
+                ButtonA20.setText(arrayA[2][1] + "");
+            }
+            if (arrayA[2][2] != 0) {
+                ButtonA21.setText(arrayA[2][2] + "");
+            }
+            if (arrayA[2][3] != 0) {
+                ButtonA22.setText(arrayA[2][3] + "");
+            }
+            if (arrayA[2][4] != 0) {
+                ButtonA23.setText(arrayA[2][4] + "");
+            }
+            if (arrayA[2][5] != 0) {
+                ButtonA24.setText(arrayA[2][5] + "");
+            }
+            if (arrayA[2][6] != 0) {
+                ButtonA25.setText(arrayA[2][6] + "");
+            }
+            if (arrayA[2][7] != 0) {
+                ButtonA26.setText(arrayA[2][7] + "");
+            }
+            if (arrayA[2][8] != 0) {
+                ButtonA27.setText(arrayA[2][8] + "");
+            }
+
+            if (arrayB[0][0] != 0) {
+                ButtonB1.setText(arrayB[0][0] + "");
+            }
+            if (arrayB[0][1] != 0) {
+                ButtonB2.setText(arrayB[0][1] + "");
+            }
+            if (arrayB[0][2] != 0) {
+                ButtonB3.setText(arrayB[0][2] + "");
+            }
+            if (arrayB[0][3] != 0) {
+                ButtonB4.setText(arrayB[0][3] + "");
+            }
+            if (arrayB[0][4] != 0) {
+                ButtonB5.setText(arrayB[0][4] + "");
+            }
+            if (arrayB[0][5] != 0) {
+                ButtonB6.setText(arrayB[0][5] + "");
+            }
+            if (arrayB[0][6] != 0) {
+                ButtonB7.setText(arrayB[0][6] + "");
+            }
+            if (arrayB[0][7] != 0) {
+                ButtonB8.setText(arrayB[0][7] + "");
+            }
+            if (arrayB[0][8] != 0) {
+                ButtonB9.setText(arrayB[0][8] + "");
+            }
+            if (arrayB[1][0] != 0) {
+                ButtonB10.setText(arrayB[1][0] + "");
+            }
+            if (arrayB[1][1] != 0) {
+                ButtonB11.setText(arrayB[1][1] + "");
+            }
+            if (arrayB[1][2] != 0) {
+                ButtonB12.setText(arrayB[1][2] + "");
+            }
+            if (arrayB[1][3] != 0) {
+                ButtonB13.setText(arrayB[1][3] + "");
+            }
+            if (arrayB[1][4] != 0) {
+                ButtonB14.setText(arrayB[1][4] + "");
+            }
+            if (arrayB[1][5] != 0) {
+                ButtonB15.setText(arrayB[1][5] + "");
+            }
+            if (arrayB[1][6] != 0) {
+                ButtonB16.setText(arrayB[1][6] + "");
+            }
+            if (arrayB[1][7] != 0) {
+                ButtonB17.setText(arrayB[1][7] + "");
+            }
+            if (arrayB[1][8] != 0) {
+                ButtonB18.setText(arrayB[1][8] + "");
+            }
+            if (arrayB[2][0] != 0) {
+                ButtonB19.setText(arrayB[2][0] + "");
+            }
+            if (arrayB[2][1] != 0) {
+                ButtonB20.setText(arrayB[2][1] + "");
+            }
+            if (arrayB[2][2] != 0) {
+                ButtonB21.setText(arrayB[2][2] + "");
+            }
+            if (arrayB[2][3] != 0) {
+                ButtonB22.setText(arrayB[2][3] + "");
+            }
+            if (arrayB[2][4] != 0) {
+                ButtonB23.setText(arrayB[2][4] + "");
+            }
+            if (arrayB[2][5] != 0) {
+                ButtonB24.setText(arrayB[2][5] + "");
+            }
+            if (arrayB[2][6] != 0) {
+                ButtonB25.setText(arrayB[2][6] + "");
+            }
+            if (arrayB[2][7] != 0) {
+                ButtonB26.setText(arrayB[2][7] + "");
+            }
+            if (arrayB[2][8] != 0) {
+                ButtonB27.setText(arrayB[2][8] + "");
+            }
+
+        });
+
 
         ButtonA1.setOnAction(event -> {
 
@@ -327,10 +508,13 @@ public class App {
         });
 
 
+    }
 
 
+    void setVal(Button button, int val) {
+        if (val != 0) {
 
-
+        }
     }
 
 }
